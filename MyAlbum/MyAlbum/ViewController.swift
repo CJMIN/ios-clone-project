d94b8c5aa8d9ffdc87a36f1da3aca7ee8720c727
CJMIN <46758264+CJMIN@users.noreply.github.com>
2020-12-31 08:02:17 -0800

    Add files via upload
    Myalbum clone project

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 최정민 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var currentValue = 0
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refresh()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func chellenge(_ sender: Any) {
+        
+        let message = "가격은 \(currentValue)입니다."
+        
+        let alert = UIAlertController(title:"Hello",message: message,preferredStyle:.alert)
+        let action = UIAlertAction(title:"OK",style: .default,handler: {action in self.refresh()})
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+        
+        
+    }
+    
+    func refresh(){
+        
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        priceLabel.text="$\(currentValue)"
+        
+    }
+    
+}
+
